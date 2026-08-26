526e65c1148d1f8ef557f9bd56581102d5ba51ed
Александр <julcapp@gmail.com>
2026-08-26 13:24:19 +0700

    android: add personal user profile model

---
+++ app/src/main/java/ru/apphelper/domain/UserProfile.kt
@@ -0,0 +1,31 @@
+package ru.apphelper.domain
+
+enum class AssistanceMode {
+    LOW_VISION,
+    SIMPLE_PHONE,
+    VOICE_FIRST
+}
+
+data class SpeechPreferences(
+    val slowerSpeech: Boolean = false,
+    val shortPhrases: Boolean = true,
+    val waitLongerForAnswer: Boolean = false,
+    val repeatImportantQuestions: Boolean = true,
+)
+
+data class UserPermissions(
+    val readNotifications: Boolean = false,
+    val callContactsWithConfirmation: Boolean = true,
+    val sendMessagesWithConfirmation: Boolean = true,
+    val shareLocationWithTrustedContactsOnly: Boolean = true,
+    val automaticRepliesEnabled: Boolean = false,
+)
+
+data class UserProfile(
+    val displayName: String = "",
+    val assistanceModes: Set<AssistanceMode> = setOf(AssistanceMode.VOICE_FIRST),
+    val speech: SpeechPreferences = SpeechPreferences(),
+    val permissions: UserPermissions = UserPermissions(),
+    val interests: Set<String> = emptySet(),
+    val onboardingCompleted: Boolean = false,
+)
